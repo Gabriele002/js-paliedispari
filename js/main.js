2d9d9c1503ef0d95752495a440912d1c1bded1e7
Gabriele002 <gabrieleciarleglio97@gmail.com>
2023-11-20 16:33:46 +0100

    finish gioco pari o dispari

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 let parola= prompt("Inserisci una parola");
 let parolaRev = reverseString(parola);
 
@@ -7,10 +8,31 @@ if (parola == parolaRev) {
 } else {
     console.log ("La parola non è palindroma")
 }
+*/
+
+//chiedere all untente di scegliere pari o dispari
+let pariDispari= prompt("Inserisci pari o dispari");
+let numeroUtente= prompt("Inserisci un numero da 1 a 5");
+
+numeroUtente = parseInt(numeroUtente);
+//generazione di due numeri casuali tra 1 e 5
+let numero1 = getRandomIntegerFromInterval(1, 5);
+//somma dei due numeri
+let sommaNumeroUtenteNumeroRandom = sommaDueNumeri (numero1, numeroUtente);
+
+if (sommaNumeroUtenteNumeroRandom % 2 == 0 && pariDispari == "pari" ) {
+    console.log("Hai vinto");
+} else if (sommaNumeroUtenteNumeroRandom % 2 == 1 && pariDispari == "dipari"){
+    console.log(" Hai vinto");
+} else {
+    console.log("Non hai vinto");
+}
+
 
 
 
 //-------------------FUNZIONI-------------------//
+/*
 function reverseString(str) {
     let newString = "";
     for (let i = str.length -1; i >= 0; i--) {
@@ -18,6 +40,16 @@ function reverseString(str) {
     }
     return newString;
 }
+*/
+function getRandomIntegerFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)+1);
+}
+function sommaDueNumeri(num1, num2) {
+    let risultato = num1 + num2;
+    return risultato;
+}
+
+
 
 
 
